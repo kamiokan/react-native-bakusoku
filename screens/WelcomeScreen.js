ca282194c3abde95244332cbb6319fd80e739e84
kamiokan <spherenov@gmail.com>
2021-03-17 17:21:43 +0900

    Welcome画面できあがり

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,47 +1,68 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Alert,
+  Dimensions,
+} from "react-native";
+import { Button } from "react-native-elements";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const SLIDE_DATA = [
-  { title: "Step: 1", text: "aaa" },
-  { title: "Step: 2", text: "bbb" },
-  { title: "Step: 3", text: "ccc" },
+  {
+    title: "Step: 1",
+    text: "Add your trip memory",
+    uri: require("../assets/welcome_screen1.jpg"),
+  },
+  {
+    title: "Step: 2",
+    text: "All tips on the list",
+    uri: require("../assets/welcome_screen2.jpg"),
+  },
+  {
+    title: "Step: 3",
+    text: "See the trip detail",
+    uri: require("../assets/welcome_screen3.jpg"),
+  },
 ];
 
 class WelcomeScreen extends React.Component {
+  onStartButtonPress = () => {
+    Alert.alert("Alert", "The button was pressed", [{ text: "OK" }], {
+      cancelable: false,
+    });
+  };
+
+  renderLastButton(index) {
+    if (index === SLIDE_DATA.length - 1) {
+      return (
+        <Button
+          style={{ padding: 10 }}
+          buttonStyle={{ backgroundColor: "deepskyblue" }}
+          title="Let's get it started!"
+          onPress={this.onStartButtonPress}
+        />
+      );
+    }
+  }
+
   renderSlides() {
     return SLIDE_DATA.map((slide, index) => {
       return (
-        <View
-          key={index}
-          style={{
-            flex: 1,
-            backgroundColor: "skyblue",
-            width: SCREEN_WIDTH,
-            alignItems: "center",
-          }}
-        >
-          <View
-            style={{
-              flex: 2,
-              backgroundColor: "pink",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Text>{slide.title}</Text>
-            <Text>{slide.text}</Text>
+        <View key={index} style={styles.slideStyle}>
+          <View style={styles.containerStyle}>
+            <Text style={styles.textStyle}>{slide.title}</Text>
+            <Text style={styles.textStyle}>{slide.text}</Text>
           </View>
 
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: "orange",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Text>{index + 1} / 3</Text>
+          <Image style={{ flex: 2 }} resizeMode="contain" source={slide.uri} />
+
+          <View style={styles.containerStyle}>
+            {this.renderLastButton(index)}
+            <Text style={styles.textStyle}>{index + 1} / 3</Text>
           </View>
         </View>
       );
@@ -56,5 +77,24 @@ class WelcomeScreen extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  slideStyle: {
+    flex: 1,
+    backgroundColor: "skyblue",
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  containerStyle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    color: "white",
+    fontSize: 20,
+    padding: 5,
+  },
+});
 
 export default WelcomeScreen;
